a494b7120fdac7c550c6f6d8ed5b43cad628d5e6
Kitae Kim <rlarlxo510@naver.com>
2025-06-02 14:30:00 +0900

    init: 프로젝트 세팅

---
+++ src/main/java/com/gamercommunity/GamerCommunityApplication.java
@@ -0,0 +1,13 @@
+package com.gamercommunity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamerCommunityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamerCommunityApplication.class, args);
+	}
+
+}
