64cd2e1df2feb92a68d1c9926d52d5ad0a20c622
Kitae Kim <rlarlxo510@naver.com>
2025-06-07 17:13:00 +0900

    feat: 인증 유틸 기능

---
+++ src/main/java/com/gamercommunity/auth/util/SecurityUtil.java
@@ -0,0 +1,24 @@
+package com.gamercommunity.auth.util;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import java.util.Optional;
+
+public class SecurityUtil {
+    private SecurityUtil() {}
+
+
+    //토큰인증 유틸
+    public static Optional<String> getCurrentLoginId() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if (authentication == null || authentication.getPrincipal() == null) {
+            return Optional.empty();
+        }
+
+        String loginId = authentication.getName();
+
+        return Optional.ofNullable(loginId);
+    }
+}
