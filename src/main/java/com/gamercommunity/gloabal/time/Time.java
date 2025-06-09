1416e45ecd34d876bf7a09feb8bb1629618e6065
Kitae Kim <rlarlxo510@naver.com>
2025-06-09 17:32:00 +0900

    feat: 글로벌 LocalDateTime 기능

---
+++ src/main/java/com/gamercommunity/gloabal/time/Time.java
@@ -0,0 +1,37 @@
+package com.gamercommunity.gloabal.time;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.PrePersist;
+
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+public class Time {
+
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+
+
+    @Column(nullable = false)
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    public void prePersist() {
+        this.updatedAt = LocalDateTime.now();
+        this.createdAt = LocalDateTime.now();
+    }
+
+
+    protected void updateTimestamp() {
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+}
+
