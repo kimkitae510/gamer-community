56508a9a3f00362c6b1b114d7c348cdcddc6ac5a
Kitae Kim <rlarlxo510@naver.com>
2025-06-14 22:04:00 +0900

    feat: 장르 삭제 API 구현

--- src/main/java/com/gamercommunity/genre/controller/GenreController.java
+++ src/main/java/com/gamercommunity/genre/controller/GenreController.java
@@ -7,10 +7,7 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/genres")
@@ -24,5 +21,12 @@ public class GenreController {
     public ResponseEntity<GenreResponse> createGenre(@RequestBody @Valid GenreRequest request) {
         GenreResponse response = genreService.createGenre(request);
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
+    // 장르 삭제
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteGenre(@PathVariable Long id) {
+        genreService.deleteGenre(id);
+        return ResponseEntity.noContent().build();
     }
 }
--- src/main/java/com/gamercommunity/genre/service/GenreService.java
+++ src/main/java/com/gamercommunity/genre/service/GenreService.java
@@ -1,6 +1,7 @@
 package com.gamercommunity.genre.service;
 
 import com.gamercommunity.exception.custom.DuplicateEntityException;
+import com.gamercommunity.exception.custom.EntityNotFoundException;
 import com.gamercommunity.genre.dto.GenreRequest;
 import com.gamercommunity.genre.dto.GenreResponse;
 import com.gamercommunity.genre.entity.Genre;
@@ -21,7 +22,7 @@ public class GenreService {
     public GenreResponse createGenre(GenreRequest request) {
         // 중복 체크
         if (genreRepository.findByName(request.name()).isPresent()) {
-            throw new DuplicateEntityException("장르중복");
+            throw new DuplicateEntityException("추가하려는 장르중복");
         }
 
         Genre genre = Genre.builder()
@@ -30,6 +31,15 @@ public class GenreService {
 
         Genre savedGenre = genreRepository.save(genre);
         return GenreResponse.from(savedGenre);
+    }
+
+    // 장르 삭제
+    @Transactional
+    public void deleteGenre(Long genreId) {
+        Genre genre = genreRepository.findById(genreId)
+                .orElseThrow(() -> new EntityNotFoundException("삭제할 장르 ID 없음"));
+
+        genreRepository.delete(genre);
     }
 }
 
