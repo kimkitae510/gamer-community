307892651a9f20005250f1839e082b47e8bfb690
Kitae Kim <rlarlxo510@naver.com>
2025-07-14 23:31:00 +0900

    feat: 회원가입, 로그인 기능 검증 추가

--- src/main/java/com/gamercommunity/user/dto/JoinRequest.java
+++ src/main/java/com/gamercommunity/user/dto/JoinRequest.java
@@ -1,6 +1,7 @@
 package com.gamercommunity.user.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,10 +13,14 @@ import lombok.Setter;
 public class JoinRequest {
 
     @NotBlank(message = "아이디가 비어있습니다.")
+    @Size(min = 8, max = 16, message = "아이디는 8자 이상 16자 이하여야 합니다.")
+    @Pattern(regexp = "^[a-zA-Z0-9]{8,16}$", message = "아이디는 영어와 숫자만 가능합니다.")
     private String loginId;
 
-    @Size(min = 8, message = "비밀번호는 8자 이상이어야 합니다")
     @NotBlank(message = "비밀번호가 비어있습니다.")
+    @Size(min = 8, max = 16, message = "비밀번호는 8자 이상 16자 이하여야 합니다.")
+    @Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,16}$", 
+             message = "비밀번호는 영어, 숫자, 특수문자를 모두 포함해야 합니다.")
     private String password;
 
     @NotBlank(message = "비밀번호 확인이 비어있습니다.")
