ec11c55f22c83e9fd94684530b8fe6a2d1c85a7a
kimkitae <145905974+kimkitae510@users.noreply.github.com>
2026-05-12 21:03:27 +0900

    Delete frontend/src/src directory

--- frontend/src/src/api/types/index.ts
+++
@@ -1,8 +0,0 @@
-// 모든 타입을 한 곳에서 export
-export * from './auth.types';
-export * from './post.types';
-export * from './comment.types';
-export * from './category.types';
-export * from './review.types';
-export * from './like.types';
-export * from './trending.types';
--- frontend/src/src/api/types/like.types.ts
+++
@@ -1,5 +0,0 @@
-// 좋아요 관련 타입 정의
-export interface LikeResponse {
-  isLiked: boolean;
-  likeCount: number;
-}
